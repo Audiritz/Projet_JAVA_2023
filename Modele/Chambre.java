273ca7da03bc86f08618eb046ad78d8125af7c1d
BaptisteMala <baptiste.malavergne@gmail.com>
2023-04-11 14:37:49 +0200

    ok

--- Modele/Chambre.java
+++ Modele/Chambre.java
@@ -8,6 +8,22 @@ public class Chambre {
     private float prix;
     private int capacite;
 
+    //constructeur par defaut
+    public Chambre(){
+        IDchambre=0;
+        monHotel=null;
+        numero=0;
+        prix=0.0F;
+        capacite=0;
 
+    }
 
+    //constructeur avec parametres
+    public Chambre(int IDchambre, Hotel monHotel, int numero, float prix, int capacite){
+        this.IDchambre=IDchambre;
+        this.monHotel=monHotel;
+        this.numero=numero;
+        this.prix=prix;
+        this.capacite=capacite;
+    }
 }
--- Modele/Hotel.java
+++ Modele/Hotel.java
@@ -7,4 +7,23 @@ public class Hotel {
     private String adresse;
     private int tel;
     private int nbEtoile;
+
+    //constructeur par defaut
+
+    public Hotel(){
+        IDhotel=0;
+        ville="";
+        adresse="";
+        tel=0;
+        nbEtoile=0;
+    }
+    
+    public Hotel(int IDhotel, String ville, String adresse, int tel, int nbEtoile){
+        this.IDhotel=IDhotel;
+        this.ville=ville;
+        this.adresse=adresse;
+        this.tel=tel;
+        this.nbEtoile=nbEtoile;
+    }
 }
+
