99feafda5052f33fbfbfac66311b43ffaa7de6f7
BaptisteMala <baptiste.malavergne@gmail.com>
2023-04-15 23:31:22 +0200

    OK

--- Vue/ListeHotel.java
+++ Vue/ListeHotel.java
@@ -1,4 +1,20 @@
 package Vue;
 
-public class ListeHotel {
+import Modele.Bdd;
+
+public class ListeHotel extends javax.swing.JInternalFrame {
+    Bdd db = new Bdd();
+
+
+
+
+    public void affichage() {
+        try{
+            String requete= "select * from hotel";
+            
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
